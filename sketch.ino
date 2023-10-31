1b3abb05aba551ec8a598c4c447a75e8fa2f6535
Leonardo Arrieta <89095474+leonardoarrieta30@users.noreply.github.com>
2023-10-31 17:05:45 -0500

    feat(iotDevice): updated data.

--- sketch.ino
+++ sketch.ino
@@ -1,112 +1,165 @@
-#include <LiquidCrystal_I2C.h>
-#include "DHTesp.h"
-#include <HTTPClient.h>
+// Include Libraries
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define Pot 35
-#define Ledpot 4
-const float Res = 4095.0;
-const float Vmax = 3.3;
-const String serverUrl = "http://localhost:8080/api/v1/iotDevice";
-#define direccion 0x27
-#define Sensor 23
-#define col 16
-#define ren 2
-#define tri 12
-#define eco 14
-LiquidCrystal_I2C lcd(direccion, col, ren);
-DHTesp DHT;
+// OLED screen size in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-class IoTData {
-  private:
-    String temperature;
-    String distance;
-    String pulse;
-    String humidity;
-  public:
-    IoTData() {}
+// Variable Declarations
+unsigned long previousMillisGetHR = 0;     // Store last time Millis (to get Heartbeat) was updated.
+unsigned long previousMillisResultHR = 0; // Store last time Millis (to get BPM) was updated.
 
-    void setTemperature(String temperature) { this->temperature = temperature; }
-    void setDistance(String distance) { this->distance = distance; }
-    void setPulse(String pulse) { this->pulse = pulse; }
-    void setHumidity(String humidity) { this->humidity = humidity; }
+const long intervalGetHR = 20;     // Interval for reading heart rate (Heartbeat) = 10ms.
+const long intervalResultHR = 10000; // The reading interval for the result of the Heart Rate calculation is in 10 seconds.
 
-    String getTemperature() { return this->temperature; }
-    String getDistance() { return this->distance; }
-    String getPulse() { return this->pulse; }
-    String getHumidity() { return this->humidity; }
+int PulseSensorSignal;      // Variable to accommodate the signal value from the sensor
+const int PulseSensorHRWire = 0;  // PulseSensor connected to ANALOG PIN 0 (A0 / ADC 0).
+const int LED_A1 = A1;            // LED to detect when the heart is beating. The LED is connected to PIN A1 on the Arduino UNO.
+int UpperThreshold = 550;         // Determine which Signal to "count as a beat", and which to ignore.
+int LowerThreshold = 500;
+
+int cntHB = 0;                     // Variable for counting the number of heartbeats.
+boolean ThresholdStat = true;     // Variable for triggers in calculating heartbeats.
+int BPMval = 0;                   // Variable to hold the result of heartbeats calculation.
+
+int x=0;            // Variable axis x graph values to display on OLED
+int y=0;            // Variable axis y graph values to display on OLED
+int lastx=0;        // The graph's last x axis variable value to display on the OLED
+int lasty=0;        // The graph's last y axis variable value to display on the OLED
+
+// 'Heart_Icon', 16x16px - I drew this heart icon at: http://dotmatrixtool.com/
+const unsigned char Heart_Icon [] PROGMEM = {
+  0x00, 0x00, 0x18, 0x30, 0x3c, 0x78, 0x7e, 0xfc, 0xff, 0xfe, 0xff, 0xfe, 0xee, 0xee, 0xd5, 0x56, 
+  0x7b, 0xbc, 0x3f, 0xf8, 0x1f, 0xf0, 0x0f, 0xe0, 0x07, 0xc0, 0x03, 0x80, 0x01, 0x00, 0x00, 0x00
 };
 
-void setup(){
-  Serial.begin(115200);
-  pinMode(Ledpot, OUTPUT);
-  lcd.init();
-  lcd.backlight();
+// Setup function
+void setup() {
+  pinMode(LED_A1,OUTPUT);  // Set LED_3 PIN as Output.
+  Serial.begin(9600);      // Set up Serial Communication at a certain speed.
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  // Address 0x3C for 128x32 and Address 0x3D for 128x64.
+  // But on my 128x64 module the 0x3D address doesn't work. What works is the 0x3C address.
+  // So please try which address works on your module.
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { 
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); //--> Don't proceed, loop forever
+  }
 
-  DHT.setup(Sensor, DHTesp::DHT22);
+  // Show initial display buffer contents on the screen
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(1000);
 
-  pinMode(tri, OUTPUT);
-  pinMode(eco, INPUT);
-  digitalWrite(tri, LOW);
-}
-void loop(){
-  int x =  analogRead(Pot);
-  float volt = (x*Vmax)/Res;
-  float luz = ((x*255)/4095);
-  analogWrite(Ledpot, luz);
-  TempAndHumidity  lectura = DHT.getTempAndHumidity();
-
-  Serial.println("Temperatura[Celcius]: " + String(lectura.temperature, 2) + "°");
-  Serial.println("Humidity: " + String(lectura.humidity, 1) + "%");
-
+  // Displays BPM value reading information
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 12); //--> (x position, y position)
+  display.print("     Please wait");
+  display.setCursor(0, 22); //--> (x position, y position)
+  display.print("     10  seconds");
+  display.setCursor(0, 32); //--> (x position, y position)
+  display.print("       to get");
+  display.setCursor(0, 42); //--> (x position, y position)
+  display.print(" the Heart Rate value");
+  display.display(); 
   delay(3000);
 
-  digitalWrite(tri, LOW);
-  digitalWrite(tri, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(tri, LOW);
-  long tiempo = pulseIn(eco, HIGH);
-  long distancia = (tiempo/29.35)/2;
-  Serial.print("Distancia cm: ");
-  Serial.println(distancia);
-  Serial.print("tiempo: ");
-  Serial.println(tiempo);
+  // Displays the initial display of BPM value
+  display.clearDisplay(); //--> for Clearing the display
+  display.drawBitmap(0, 47, Heart_Icon, 16, 16, WHITE); //--> display.drawBitmap(x position, y position, bitmap data, bitmap width, bitmap height, color)
+  display.drawLine(0, 43, 127, 43, WHITE); //--> drawLine(x1, y1, x2, y2, color)
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(20, 48); //--> (x position, y position)
+  display.print(": 0 BPM");
+  display.display(); 
 
-  lcd.setCursor(1, 0);
-  lcd.print("Pulso cardiaco");
-  lcd.setCursor(6, 1);
-  lcd.print(volt);
-  delay(1000);
-  lcd.clear();
-  delay(100);
+  Serial.println();
+  Serial.println("Please wait 10 seconds to get the BPM Value");
+}
+void loop() {
+  GetHeartRate(); // Calling the GetHeartRate() subroutine
+}
 
-  lcd.setCursor(0, 0);
-  lcd.print("Humedad "+ String(lectura.humidity, 1));
-  lcd.setCursor(0, 1);
-  lcd.print("Temperatura "+ String(lectura.temperature, 2));
-  delay(1000);
-  lcd.clear();
-  delay(100);
+void GetHeartRate() {
+  // Process of reading heart rate.
+  unsigned long currentMillisGetHR = millis();
 
-  lcd.setCursor(0, 0);
-  lcd.print("Distancia ");
-  lcd.setCursor(0, 1);
-  lcd.print(distancia);
-  delay(1000);
-  lcd.clear();
-  delay(10);
+  if (currentMillisGetHR - previousMillisGetHR >= intervalGetHR) {
+    previousMillisGetHR = currentMillisGetHR;
 
-  // Crear un objeto IoTData y asignar los datos
-  IoTData data;
-  data.setTemperature(String(lectura.temperature, 2));
-  data.setDistance(String(distancia));
-  data.setPulse(String(volt));
-  data.setHumidity(String(lectura.humidity, 1));
+    PulseSensorSignal = analogRead(PulseSensorHRWire);
 
-  // Crear un objeto JSON con los datos
-  String objetData = 
-  "{\"temperature\":" + data.getTemperature() + 
-  ",\"distance\":" + data.getDistance() + 
-  ",\"pulse\":" + data.getPulse() + 
-  ",\"humidity\":" + data.getHumidity() + "}";
+    if (PulseSensorSignal > UpperThreshold && ThresholdStat == true) {
+      cntHB++;
+      ThresholdStat = false;
+      digitalWrite(LED_A1, HIGH);
+    }
+
+    if (PulseSensorSignal < LowerThreshold) {
+      ThresholdStat = true;
+      digitalWrite(LED_A1, LOW);
+    }
+
+    DrawGraph(); // Calling the DrawGraph() subroutine
+  }
+
+  // Process for getting the BPM value.
+  unsigned long currentMillisResultHR = millis();
+
+  if (currentMillisResultHR - previousMillisResultHR >= intervalResultHR) {
+    previousMillisResultHR = currentMillisResultHR;
+
+    BPMval = cntHB * 6; // The taken heart rate is for 10 seconds. So to get the BPM value, the total heart rate in 10 seconds x 6.
+    Serial.print("BPM : ");
+    Serial.println(BPMval);
+
+    display.fillRect(20, 48, 108, 18, BLACK);
+
+    display.drawBitmap(0, 47, Heart_Icon, 16, 16, WHITE);
+
+    display.drawLine(0, 43, 127, 43, WHITE);
+
+    display.setTextSize(2);
+    display.setTextColor(WHITE);
+    display.setCursor(20, 48);
+    display.print(": ");
+    display.print(BPMval);
+    display.print(" BPM");
+    display.display();
+
+    cntHB = 0;
+  }
+}
+
+void DrawGraph() {
+  // Condition to reset the graphic display if it fills the width of the OLED screen
+  if (x > 127) {
+    display.fillRect(0, 0, 128, 42, BLACK);
+    x = 0;
+    lastx = 0;
+  }
+
+  // Process signal data to be displayed on OLED in graphic form
+  int ySignal = PulseSensorSignal;
+  if (ySignal > 850) ySignal = 850;
+  if (ySignal < 350) ySignal = 350;
+  int ySignalMap = map(ySignal, 350, 850, 0, 40);
+  y = 40 - ySignalMap;
+  // Displays the heart rate graph
+  display.writeLine(lastx, lasty, x, y, WHITE);
+  display.display();
+  lastx = x;
+  lasty = y;
+  x++;
 }
