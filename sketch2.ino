a4ce699e109f573f2abb10e7fbf63f842a35b933
Luis Pineda <89112808+LuisPineda05@users.noreply.github.com>
2023-11-14 10:55:53 -0500

    feat(degrees): aded read en C andF

--- sketch2.ino
+++ sketch2.ino
@@ -1,141 +1,62 @@
+#include "DHT.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-#include <dht.h>
-#include <TinyWireM.h>
-#include <Tiny4kOLED.h>
+const char* ssid = "Wokwi-GUEST";
+const char* password = "";
 
-#define DHT22_PIN PB1
-
-const unsigned char  img_heart_small[] PROGMEM = {
-  0x00, 0x00, 0xc0, 0xe0, 0xe0, 0xe0, 0xc0, 0x80, 0x80, 0x80, 0xc0, 0xe0, 0xe0, 0xe0, 0xc0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x1f, 0x0f, 0x07, 0x03, 0x01, 0x00, 0x00, 0x00
-};
-
-const unsigned char  img_heart_big[] PROGMEM = {
-  0xe0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf8, 0xf0, 0xe0, 0xe0, 0xe0, 0xf0, 0xf8, 0xf8, 0xf8, 0xf8, 0xf0, 0xe0, 0x00, 0x01, 0x03, 0x07, 0x0f, 0x1f, 0x3f, 0x7f, 0xff, 0x7f, 0x3f, 0x1f, 0x0f, 0x07, 0x03, 0x01, 0x00
-};
-
-const unsigned char  img_thermometer[] PROGMEM = {
-  0x00, 0xfe, 0x03, 0xfe, 0x50,
-  0x00, 0xff, 0x00, 0xff, 0x55,
-  0x60, 0x9f, 0x80, 0x9f, 0x65,
-};
-
-dht DHT;
-
-void splash() {
-  oled.clear();
-  oled.setCursor(15, 1);
-  oled.print(F("ATtiny85+SSD1306"));
-
-  oled.setCursor(42, 3);
-  oled.print(F("Example"));
-
-  oled.setCursor(35, 7);
-  oled.print(F("wokwi.com"));
-}
-
-void heartBeat() {
-  static char big = 1;
-  static long startTime = 0;
-  long currentTime;
-
-  // Get current time
-  currentTime = millis();
-
-  // Update if 200ms passed
-  if ((currentTime - startTime) > 200) {
-    startTime = currentTime;
-    big = 1 - big;
-
-    if (big) {
-      oled.bitmap(20, 4, 37, 6, img_heart_big);
-    } else {
-      oled.bitmap(20, 4, 37, 6, img_heart_small);
-    }
-  }
-}
-
-void prepareDisplay() {
-  unsigned int i, k;
-  unsigned char ch[5];
-
-  oled.clear();
-  oled.begin();
-
-  oled.setCursor(20, 1);
-  oled.print(F("ATtiny85+SSD1306"));
-  oled.setCursor(3, 2);
-  oled.print(F("temperature|humidity"));
-
-  oled.bitmap(105, 4, 110, 7, img_thermometer);
-  oled.setCursor(57, 4);
-  oled.print(F("24.0C"));
-  oled.setCursor(57, 5);
-  oled.print(F("40.0%"));
-
-  oled.bitmap(10, 5, 17, 2, img_heart_small);
-}
-
-float getTemperature() {
-  return DHT.temperature;
-}
-
-float getHumidity() {
-  return DHT.humidity;
-}
+#define DHTPIN 16   
+#define DHTTYPE DHT22 
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
-  pinMode(DHT22_PIN, INPUT);
 
-  oled.begin(128, 64, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
-
-  // Two fonts are supplied with this library, FONT8X16 and FONT6X8
-  oled.setFont(FONT6X8);
-
-  // To clear all the memory
-  oled.clear();
-  oled.on();
-
-  splash();
-
-  delay(3000);
-  prepareDisplay();
+  Serial.begin(9600);
+  Serial.print("Connecting to WiFi");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
 }
 
 void loop() {
-  static long startTime = 0;
-  long currentTime;
+  float humidity = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float degreesC = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float degreesF = dht.readTemperature(true);
 
-  DHT.read22(DHT22_PIN);
+  String humidityString = String(humidity) + "%";
+  String degreesCString = String(degreesC) + " °C";
+  String degreesFString = String(degreesF) + " °F";
+  String pulse = "80 BPM";
 
-  // Get current time
-  currentTime = millis();
+ // Construye el JSON manualmente
+  String jsonStr = "{\"temperature\":\"" +degreesCString   + "\"" +
+                  ",\"distance\":\"" + degreesFString + "\"" +
+                  ",\"pulse\":\"" + pulse + "\"" +
+                  ",\"humidity\":\"" + humidityString  + "\"}";
 
-  // Checks 1 second passed
-  if ((currentTime - startTime) > 1000) {
-    startTime = currentTime;
 
-    // Update temperature
-    float temperature = getTemperature();
+  // Crea la solicitud HTTP
+  HTTPClient http;
+  http.begin("https://api-iotheraphy-production-909e.up.railway.app/api/v1/iotDevice"); // Reemplaza con la URL de tu endpoint
+  http.addHeader("Content-Type", "application/json");
 
-    // Set cursor
-    oled.setCursor(57, 4);
+  // Envía la solicitud POST con los datos JSON
+  int httpCode = http.POST(jsonStr);
+if (httpCode == HTTP_CODE_CREATED) {
+  Serial.println("Solicitud exitosa. Datos enviados.");
+} else {
+  Serial.print("Error en la solicitud. Código de estado: ");
+  Serial.println(httpCode);
+  String errorMsg = http.errorToString(httpCode);
+  Serial.println("Error: " + errorMsg);
+}
 
-    // Print to display
-    oled.print(temperature, 1);
-    oled.print("C  ");
+  http.end();
 
-    // Update humidity
-    float humidity = getHumidity();
-
-    // Set cursor
-    oled.setCursor(57, 5);
-
-    // Print to display
-    oled.print(humidity, 1);
-    oled.print("%  ");
-
-    oled.bitmap(105, 4, 110, 7, img_thermometer);
-  }
-
-  heartBeat();
+  delay(1000); // Espera un segundo antes de volver a enviar los datos
 }
